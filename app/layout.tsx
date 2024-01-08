0f321657c8c7fd369ba2ec835550b18cf68dd043
Rangga056 <muhammadrangga056@gmail.com>
2024-01-08 10:04:37 +0700

    Finish Header and user auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -23,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,17 +1,44 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className='w-full border-b'>
+    <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className='w-36'>
-          <Image src="/assets/images/logo.svg" alt='logo' width={128} height={38} />
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/eclipse.svg"
+            alt="Eclipse Summit logo"
+            width={128}
+            height={38}
+          />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
