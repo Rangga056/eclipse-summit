775049817b45a82ff038a762e824cccc9e1ba481
Rangga056 <muhammadrangga056@gmail.com>
2024-01-09 08:02:52 +0700

    create the landing page and create database models

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src="/assets/images/eclipse.svg" alt="logo" width={128} height={38} className='ml-7 sm:ml-0'/>
+        </Link>
+
+        <p className="">2024 Eclipse Summit. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
