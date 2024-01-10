c89e4587dadbb0299bc35fe75a2f717e2ef29eaa
Rangga056 <muhammadrangga056@gmail.com>
2024-01-10 13:01:13 +0700

    Create event form component UI

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,8 +56,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
-      evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -9,9 +9,7 @@ export const connectToDatabase = async () => {
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
       dbName: "eclipse-summit",
       bufferCommands: false,
     });
