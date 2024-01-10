4a3f04f0008637b1327b57a78c297a6ebbcc4961
Rangga056 <muhammadrangga056@gmail.com>
2024-01-10 15:01:57 +0700

    create individual event page ui

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,9 +1,20 @@
 "use server";
 import { connectToDatabase } from "../database";
+import Category from "../database/models/category.model";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
 import { handleError } from "../utils";
 import { CreateEventParams } from "./../../types/index";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -26,7 +37,22 @@ export const createEvent = async ({
     });
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
 
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
