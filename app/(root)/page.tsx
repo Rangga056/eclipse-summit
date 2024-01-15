c5f378b9cfa58af2f756648c19e45183abf6caad
Rangga056 <muhammadrangga056@gmail.com>
2024-01-15 12:02:21 +0700

    complete search and category filter function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
@@ -52,7 +53,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
-          CategoryFilter
+          <CategoryFilter />
         </div>
 
         <Collection
